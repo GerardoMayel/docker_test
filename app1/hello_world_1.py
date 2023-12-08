9c0ec41832ebd93473f83258a415b253b17b8682
Gerardo Mayel Fernández Alamilla <geramfernandez@gmail.com>
2023-12-08 12:02:57 -0600

    se modifica docker file ya que la carpeta de logs ya existe y se modifica script de python para capturar las librerias en el log

--- app1/hello_world_1.py
+++ app1/hello_world_1.py
@@ -8,18 +8,19 @@ logging.basicConfig(filename='/app/logs/app1.log', level=logging.INFO, format='%
 # Lee la variable de entorno 'MESSAGE'. Si no está definida, usa 'Hello World 1' como predeterminado.
 message = os.getenv('MESSAGE', 'Hello World 1 no definido mensaje 1')
 
-# Registra el mensaje en el archivo de log
+# Registra el mensaje en el archivo de log y lo imprime en la consola
 logging.info(message)
-
-# Imprime el mensaje en la consola
 print(message)
 
-#Imprime Hello World
+# Imprime y registra "Hello World"
 print("Hello World 1")
 logging.info("Hello World 1")
 
-#Imprime el resultado de pip freeze
-subprocess.call(['pip', 'freeze'])
+# Ejecuta pip freeze y captura la salida
+try:
+    requirements_output = subprocess.check_output(['pip', 'freeze'], text=True)
+    print(requirements_output)
+    logging.info(requirements_output)
+except subprocess.CalledProcessError as e:
+    logging.error("Error al ejecutar pip freeze: " + str(e))
 
-#guarda el resultado de pip freeze en un archivo
-logging.info(subprocess.call(['pip', 'freeze']))
--- app2/hello_world_2.py
+++ app2/hello_world_2.py
@@ -5,24 +5,25 @@ import subprocess
 # Configuración de logging
 logging.basicConfig(filename='/app/logs/app1.log', level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
-# Lee la variable de entorno 'MESSAGE'. Si no está definida, usa 'Hello World 1' como predeterminado.
+# Lee la variable de entorno 'MESSAGE'. Si no está definida, usa 'Hello World 2' como predeterminado.
 message = os.getenv('MESSAGE', 'Hello World 2 no definido mensaje 2')
 
-# Registra el mensaje en el archivo de log
+# Registra el mensaje en el archivo de log y lo imprime en la consola
 logging.info(message)
-
-# Imprime el mensaje en la consola
 print(message)
 
-#Imprime Hello World
+# Imprime y registra "Hello World"
 print("Hello World 2")
 logging.info("Hello World 2")
 
-#Imprime el resultado de pip freeze
-subprocess.call(['pip', 'freeze'])
+# Ejecuta pip freeze y captura la salida
+try:
+    requirements_output = subprocess.check_output(['pip', 'freeze'], text=True)
+    print(requirements_output)
+    logging.info(requirements_output)
+except subprocess.CalledProcessError as e:
+    logging.error("Error al ejecutar pip freeze: " + str(e))
 
-#guarda el resultado de pip freeze en un archivo
-logging.info(subprocess.call(['pip', 'freeze']))
 
 
 
